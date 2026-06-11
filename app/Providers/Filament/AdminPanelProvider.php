2039b7ee96eb02dc76c3e70a687dbdeab9b8dedd
Ahmed-Al-Hemyari <ahmed.a.alhemyari@gmail.com>
2026-06-11 17:55:28 +0300

    enhancing admin panel UI

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,6 +11,7 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\Width;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -30,12 +31,19 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Hex('#941b1d'),
             ])
+            ->font('Cairo')
+            // ->brandLogo('/storage/masar-light.svg')
+            // ->darkModeBrandLogo('/storage/masar-dark.svg')
+            ->brandLogoHeight('40px')
+            ->maxContentWidth(Width::Full)
+            ->readOnlyRelationManagersOnResourceViewPagesByDefault(false)
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
-                Dashboard::class,
+                // Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
