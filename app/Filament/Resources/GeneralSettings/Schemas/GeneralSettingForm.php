dcca088354efc6490b6d1d66ca7e572a32dfbf05
Ahmed-Al-Hemyari <ahmed.a.alhemyari@gmail.com>
2026-06-26 13:18:59 +0300

    bugs fixes with general setting form

--- app/Filament/Resources/GeneralSettings/Schemas/GeneralSettingForm.php
+++ app/Filament/Resources/GeneralSettings/Schemas/GeneralSettingForm.php
@@ -21,22 +21,9 @@ class GeneralSettingForm
                             ->required(),
                         TextInput::make('value')
                             ->label(__('English Value'))
-                            ->placeholder('#000000')
-                            ->helperText(__('Enter the color hex code starting with # (e.g., #000000)'))
-                            ->regex('/^#[a-fA-F0-9]{6}$/')
-                            ->validationMessages([
-                                'regex' => __('The color format must be a valid hex code like #000000.'),
-                            ])
                             ->required(),
                         TextInput::make('value_ar')
-                            ->label(__('Arabic Value'))
-                            ->placeholder('#000000')
-                            ->helperText(__('Enter the color hex code starting with # (e.g., #000000)'))
-                            ->regex('/^#[a-fA-F0-9]{6}$/')
-                            ->validationMessages([
-                                'regex' => __('The color format must be a valid hex code like #000000.'),
-                            ])
-                            ->required(),
+                            ->label(__('Arabic Value')),
                         Checkbox::make('is_published')
                             ->label(__('Published')),
                     ])
